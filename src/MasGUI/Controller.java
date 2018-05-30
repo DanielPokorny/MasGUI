dbcf626dc4fbf7d0ba40114b59ab410593b44441
daniel <mr.pokorny.d@gmail.com>
2018-05-30 11:04:57 +0200

    Reaguje na novy text

--- src/MasGUI/Controller.java
+++ src/MasGUI/Controller.java
@@ -1,4 +1,15 @@
 package MasGUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField inputField;
+    @FXML
+    private TextArea outputField;
+    public void processCommand() {
+        outputField.setText(outputField.getText() + inputField.getText() + System.getProperty("line.separator"));
+    }
 }
